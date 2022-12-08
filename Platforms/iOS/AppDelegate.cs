e6a8f93ea68eb7d131f7cea469bdd495eab04c42
Isaac Zais <isaaczais@hotmail.com>
2022-12-07 17:47:58 -0800

    Add project files.

---
+++ Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace StompingGrounds;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
